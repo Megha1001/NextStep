7f3a14ede19a59a872ecfee4a3eba9cd85d7486b
Megha Verma <meghaverma1001@gmail.com>
2025-12-04 18:11:07 +0530

    Create NaiveSolution.java

---
+++ mathematics/palindrome/NaiveSolution.java
@@ -0,0 +1,26 @@
+/*
+ * Question : Find whether the number is palindrome or not ?
+ */
+
+package mathematics.palindrome;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int n = 1234321;
+        System.out.println("is the given number palindrome  : ? "+isPalindrome(n));
+    }
+    
+
+    public static boolean isPalindrome(int n){
+        int rev = 0;
+        int temp = n;
+        while(n!=0){
+            rev = rev*10 + (n%10);
+            n = n/10;
+        }
+
+        return temp == rev;
+    }
+
+}
