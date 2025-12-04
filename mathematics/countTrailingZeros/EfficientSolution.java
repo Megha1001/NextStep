8c544adc2b101a1ba64432a076a9c6e29b70372d
Megha Verma <meghaverma1001@gmail.com>
2025-12-04 20:09:48 +0530

    Create EfficientSolution.java

---
+++ mathematics/countTrailingZeros/EfficientSolution.java
@@ -0,0 +1,29 @@
+package mathematics.countTrailingZeros;
+
+/*
+ * Idea : We need to find how many 2, 5 combination are there in prime factorization of the number
+ * => Num of 5 < num of 2 
+ * => find number of 5
+ */
+
+public class EfficientSolution {
+
+
+    public static void main(String args[]){
+        int n = 251;
+        System.out.println("Number of trailing zeros in factorial of number are : " + findTrailingZeroInFactorial(n));
+
+    }
+
+    public static int findTrailingZeroInFactorial(int n ){
+
+        int res = 0;
+        
+        for(int i = 5; i <= n; i = i*5){
+            res += (n/i);
+        }
+
+        return res;
+    }
+    
+}
