34501adedd6d6681aaa67640183e38ab5fd6ebda
Megha Verma <meghaverma1001@gmail.com>
2025-12-05 20:49:38 +0530

    Create EuclidienAlgo.java

---
+++ mathematics/gcdOrHcf/EuclidienAlgo.java
@@ -0,0 +1,23 @@
+package mathematics.gcdOrHcf;
+
+public class EuclidienAlgo {
+
+    public static void main(String args[]){
+        int n1 = 4, n2 = 6;
+        System.out.println("GCD of given  numbers are : "+findGCD(n1, n2));
+    }
+
+    public static int findGCD(int n1, int n2){
+
+        while(n1 != n2){
+            if(n1 > n2){
+                n1 = n1 - n2;
+            }else {
+                n2 = n2 - n1;
+            }
+        }
+
+        return n1;
+    }
+    
+}
