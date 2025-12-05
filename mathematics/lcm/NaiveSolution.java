4c770cc417814a228c90a6939c844c91e6ec7b00
Megha Verma <meghaverma1001@gmail.com>
2025-12-05 21:33:40 +0530

    Create NaiveSolution.java

---
+++ mathematics/lcm/NaiveSolution.java
@@ -0,0 +1,32 @@
+package mathematics.lcm;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int a = 3, b = 7;
+        System.out.println("LCM of given number is : "+findLCM(a, b));
+    }
+
+    public static int findLCM(int a, int b){
+        int res = a*b;
+
+        //base cases
+        if(a%b==0){
+            return a;
+        }
+
+        if(b%a == 0){
+            return b;
+        }
+
+
+        for(int i = 2; i < a*b; i++){
+            if(i%a == 0 && i%b == 0 ){
+                return i;
+            }
+        }
+
+        return res;
+    }
+    
+}
