21db177481d40059441818f2be66a73d0cc9dffd
Megha Verma <meghaverma1001@gmail.com>
2025-12-05 21:12:09 +0530

    Update OptimizedEuclideanAlgorithm.java

---
+++ mathematics/gcdOrHcf/OptimizedEuclideanAlgorithm.java
@@ -0,0 +1,19 @@
+package mathematics.gcdOrHcf;
+
+public class OptimizedEuclideanAlgorithm {
+
+    public static void main(String args[]){
+        int a = 4, b = 6;
+        System.out.println("GCD of two given numbers is : "+findGCD(a,b));
+    }
+
+    public static int findGCD(int a, int b){
+
+        if(b==0){
+            return a;
+        }
+
+        return findGCD(b, a%b);
+    }
+    
+}
