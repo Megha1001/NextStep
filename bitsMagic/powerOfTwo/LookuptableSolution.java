0facf474a837109db90bcee77f0bbd45e7dd2e60
Megha Verma <meghaverma1001@gmail.com>
2025-12-06 17:42:27 +0530

    Create LookuptableSolution.java

---
+++ bitsMagic/powerOfTwo/LookuptableSolution.java
@@ -0,0 +1,32 @@
+package bitsMagic.powerOfTwo;
+
+public class LookuptableSolution {
+
+    private static int tbl[] = new int[256];
+
+    public static void main(String args[]){
+        populateTbl();
+        int n = 16;
+        System.out.println("is given number is power of 2 ? "+isPowerOfTwo(n));
+    }
+
+
+    public static void populateTbl(){
+
+        tbl[0] = 0;
+        for(int i = 1; i < 256; i++){
+            tbl[i] = tbl[i & (i-1)] + 1;
+        }
+
+    }
+
+    public static boolean isPowerOfTwo(int n){
+
+        return (tbl[n & 255]
+        + tbl[(n >> 8) & 255]
+        + tbl[(n >> 16) & 255]+
+        tbl[n>>24]) ==1;
+
+    }
+    
+}
