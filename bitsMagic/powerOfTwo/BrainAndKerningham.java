5d6576421bd909ece4de7f80cfb798c4c543674f
Megha Verma <meghaverma1001@gmail.com>
2025-12-06 17:38:43 +0530

    Create BrainAndKerningham.java

---
+++ bitsMagic/powerOfTwo/BrainAndKerningham.java
@@ -0,0 +1,27 @@
+package bitsMagic.powerOfTwo;
+
+/*
+ * Idea : One set bit should be there
+ */
+
+public class BrainAndKerningham {
+
+    public static void main(String args[]){
+        int n = 16;
+        System.out.println("Is the given number is power of 2 ? "+isPowerOf2(n));
+    }
+
+    public static boolean isPowerOf2(int n ){
+        int res = 0;
+        while(n > 0){
+            n = n & (n-1);
+            ++res;
+            if(res > 1){
+                return false;
+            }
+        }
+
+        return res==1;
+    }
+    
+}
