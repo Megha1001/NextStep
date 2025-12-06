fc3370c0f12de06441096db9685725455c08b9c0
Megha Verma <meghaverma1001@gmail.com>
2025-12-06 19:19:31 +0530

    Create NaiveSolution.java

---
+++ bitsMagic/twoOddOccurrence/NaiveSolution.java
@@ -0,0 +1,32 @@
+package bitsMagic.twoOddOccurrence;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int n[] = {5,6,10,6,10,6,3,3};
+        System.out.println("Numbers that occurred odd number of times are :- ");
+        findOccurrence(n);
+    }
+
+    public static void findOccurrence(int n[]){
+
+        boolean [] visited = new boolean[n.length];
+
+        for(int i=0; i<n.length; i++){
+            if(visited[i]) continue;
+            int temp=0;
+            for(int j=0; j<n.length; j++){
+                if(n[i] == n[j]){
+                    ++temp;
+                    visited[j] = true;
+                }
+            }
+
+            if(temp%2 != 0){
+                System.out.print(n[i]+" ");
+            }
+        }
+
+    }
+    
+}
