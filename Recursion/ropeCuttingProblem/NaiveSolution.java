9e192a2688f191a07546fe6c17f124af8dcfacf7
Megha Verma <meghaverma1001@gmail.com>
2025-12-07 16:10:57 +0530

    Update NaiveSolution.java

--- Recursion/ropeCuttingProblem/NaiveSolution.java
+++ Recursion/ropeCuttingProblem/NaiveSolution.java
@@ -4,27 +4,34 @@ public class NaiveSolution {
 
     public static void main(String args[]){
         int n = 9, a = 2, b = 2, c = 2;
-        System.out.println("Max cuts that we can make is : "+findMaxCut(n, a, b, c, 0));
+        System.out.println("Max cuts that we can make is : "+findMaxCut(n, a, b, c));
     }
 
 
-    public static int findMaxCut(int n, int a, int b, int c, int l){
+    public static int findMaxCut(int n, int a, int b, int c){
 
         if(n==0){
-            return l;
+            return 0;
         }
 
         if(n < 0){
             return -1;
         }
 
-        return Math.max(
+        int res =Math.max(
             Math.max(
-                findMaxCut(n-a, a, b, c, l+1),
-                findMaxCut(n-b, a, b, c, l+1)
+                findMaxCut(n-a, a, b, c),
+                findMaxCut(n-b, a, b, c)
             ),
-            findMaxCut(n-c, a, b, c, l+1)
+            findMaxCut(n-c, a, b, c)
         );
+
+        if(res == -1){
+                return -1;
+        }
+
+        //Only add 1 to the valid cases
+        return res + 1;
 
     }
     
