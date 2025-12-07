9d9bae4923f2d0c79df4e18f15f5b1188580fb70
Megha Verma <meghaverma1001@gmail.com>
2025-12-07 14:52:54 +0530

    Create SumOfDigitsUsingRecursion.java

---
+++ Recursion/sumOfDigits/SumOfDigitsUsingRecursion.java
@@ -0,0 +1,19 @@
+package Recursion.sumOfDigits;
+
+public class SumOfDigitsUsingRecursion {
+
+    public static void main(String arr[]){
+        int n = 253;
+        System.out.println("SUm of digits in given number is : "+findSum(n));
+    }
+
+    public static int findSum(int n){
+
+        if(n==0){
+            return 0;
+        }
+
+        return (n%10) + findSum(n/10);
+    }
+    
+}
