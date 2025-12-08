7011a98f53c8cdf2a6ba4ee793891a2191dc4f58
Megha Verma <meghaverma1001@gmail.com>
2025-12-08 22:13:09 +0530

    Create JosephusProblem.java

---
+++ Recursion/josephusProblem/JosephusProblem.java
@@ -0,0 +1,21 @@
+package Recursion.josephusProblem;
+
+public class JosephusProblem {
+
+    public static void main(String args[]){
+        int n = 5, k=3;
+
+        System.out.println("The survived person index is : "+findSurvivedPersonIdx(n,k));
+    }
+
+    public static int findSurvivedPersonIdx(int n, int k){
+
+        if(n==1){
+            return 0;
+        }
+
+        return (k+findSurvivedPersonIdx(n-1, k)) %n;
+
+    }
+    
+}
