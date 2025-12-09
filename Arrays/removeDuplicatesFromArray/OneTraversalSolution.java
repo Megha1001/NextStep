ae7b2f7eec26ccc68aeb9c0070fb792a68f8f30e
Megha Verma <meghaverma1001@gmail.com>
2025-12-09 21:36:31 +0530

    Create OneTraversalSolution.java

---
+++ Arrays/removeDuplicatesFromArray/OneTraversalSolution.java
@@ -0,0 +1,39 @@
+package Arrays.removeDuplicatesFromArray;
+
+public class OneTraversalSolution {
+
+    public static void main(String args[]){
+        int arr [] = {10,20,30};
+        int n = arr.length;
+        int newLength = findSizeOfArray(arr,n);
+        System.out.println("Size of array after removing duplicates is : "+findSizeOfArray(arr,n));
+        printArray(arr, newLength);
+    }
+
+
+    public static int findSizeOfArray(int arr[], int n){
+        
+        int i = 0, j=1, curr=1;
+
+        while(i < n-1){
+
+            if(arr[i] != arr[j]){
+                arr[curr] = arr[j];
+                ++curr;
+            }
+
+            ++i;
+            ++j;
+
+        }
+
+        return curr;
+    }
+
+    public static void printArray(int arr[], int n){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
