72878d90659a694e3ba29f069c2ee6e10c6cda97
Megha Verma <meghaverma1001@gmail.com>
2025-12-09 20:37:00 +0530

    Create CheckIfArrayIsSorted.java

---
+++ Arrays/checkArrayIsSorted/CheckIfArrayIsSorted.java
@@ -0,0 +1,25 @@
+package Arrays.checkArrayIsSorted;
+
+public class CheckIfArrayIsSorted {
+
+
+    public static void main(String args[]){
+        int arr[] = {100,60,80};
+
+        System.out.println("is the given array sorted ? : "+isSorted(arr));
+    }
+
+    public static boolean isSorted(int arr[]){
+
+        for(int i=1; i<arr.length; i++){
+
+            if(arr[i-1] > arr[i]){
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+    
+}
