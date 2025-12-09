da58206763cad8bd7286959bff7a77f6dfb0c66f
Megha Verma <meghaverma1001@gmail.com>
2025-12-09 23:03:13 +0530

    Create EfficientSolution2.java

---
+++ Arrays/movingZerosToEnd/EfficientSolution2.java
@@ -0,0 +1,38 @@
+package Arrays.movingZerosToEnd;
+
+public class EfficientSolution2 {
+
+
+    public static void main(String args[]){
+        int arr[] = {8,5,0,10,0,20};
+        int n = arr.length;
+        System.out.println("Array after moving zeros to end is : ");
+        moveZerosToEnd(arr, n);
+        printArray(arr, n);
+    }
+
+
+    public static void moveZerosToEnd(int arr[], int n){
+        int count=0;
+        for(int i = 0; i<n; i++){
+            if(arr[i]!=0){
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                ++count;
+            }
+        }
+    }
+
+
+    public static void printArray(int arr[], int n){
+        for(int i=0; i< n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    
+}
