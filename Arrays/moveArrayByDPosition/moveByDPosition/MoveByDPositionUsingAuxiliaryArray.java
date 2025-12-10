99701de317798376d8fe2b94977d3b28f308ace9
Megha Verma <meghaverma1001@gmail.com>
2025-12-10 21:59:08 +0530

    Create MoveByDPositionUsingAuxiliaryArray.java

---
+++ Arrays/moveArrayByDPosition/moveByDPosition/MoveByDPositionUsingAuxiliaryArray.java
@@ -0,0 +1,43 @@
+package Arrays.moveArrayByDPosition.moveByDPosition;
+
+public class MoveByDPositionUsingAuxiliaryArray {
+
+
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        int n = arr.length;
+        int d=2;
+        System.out.println("Array after moving  by d position");
+        movingArrayByDPosition(arr, n, d);
+        printArray(arr, n);
+    }
+
+    public static void movingArrayByDPosition(int arr[], int n, int d){
+
+        int temp [] = new int[d];
+
+        //copy elements from arr[0] to arr[d] into temp
+        for(int i=0; i<d; i++){
+            temp[i] = arr[i];
+        }
+
+        //move element from d to n-1 to 0 to n-1-d
+        for(int i=d; i<n; i++){
+            arr[i-d] = arr[i];
+        }
+
+        //move elements from temp to arr
+        for(int i=0; i<d; i++){
+            arr[n-d+i]= temp[i];
+            
+        }
+
+    }
+
+    public static void printArray(int arr[], int n){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    
+}
