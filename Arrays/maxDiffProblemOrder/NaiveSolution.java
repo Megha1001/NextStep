fcb7425149e2b9cb643bd81f6c09ef2c366b9b77
Megha Verma <meghaverma1001@gmail.com>
2025-12-10 23:51:36 +0530

    Create NaiveSolution.java

---
+++ Arrays/maxDiffProblemOrder/NaiveSolution.java
@@ -0,0 +1,31 @@
+package Arrays.maxDiffProblemOrder;
+
+/*
+ * max value of arr[j] - arr[i] such that j>i
+ */
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {30,10,8,-2};
+        System.out.println("Max difference is : "+findMaxDiff(arr, arr.length));
+    }
+
+    public static int findMaxDiff(int arr[], int n){
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                int diff = arr[j]-arr[i];
+                if(diff> max){
+                    max = diff;
+                }
+            }
+        }
+
+        return max;
+
+    }
+    
+}
