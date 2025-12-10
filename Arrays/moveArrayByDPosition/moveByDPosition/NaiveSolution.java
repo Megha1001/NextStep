7991d337c16d1fc753c5cf584749deb5d1ce1abe
Megha Verma <meghaverma1001@gmail.com>
2025-12-10 21:34:35 +0530

    Create NaiveSolution.java

---
+++ Arrays/moveArrayByDPosition/moveByDPosition/NaiveSolution.java
@@ -0,0 +1,32 @@
+package Arrays.moveArrayByDPosition.moveByDPosition;
+
+public class NaiveSolution {
+
+
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        int n = arr.length;
+        int d=2;
+        System.out.println("Array after moving  by d position");
+        movingArrayByDPosition(arr, n, d);
+        printArray(arr, n);
+    }
+
+    public static void movingArrayByDPosition(int arr[], int n, int d){
+
+        for(int i=0; i<d; i++){
+            int temp = arr[0];
+            for(int j=1; j<n; j++){
+                arr[j-1] = arr[j];
+            }
+            arr[n-1] = temp;
+        }
+    }
+
+    public static void printArray(int arr[], int n){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    
+}
