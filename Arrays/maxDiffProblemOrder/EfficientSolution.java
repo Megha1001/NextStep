1b8a1a24909f13bbc7cd6b87cd15b52de6c10e30
Megha Verma <meghaverma1001@gmail.com>
2025-12-11 00:21:20 +0530

    Create EfficientSolution.java

---
+++ Arrays/maxDiffProblemOrder/EfficientSolution.java
@@ -0,0 +1,24 @@
+package Arrays.maxDiffProblemOrder;
+
+public class EfficientSolution {
+
+    public static void main(String args[]){
+        int arr[] = {30,10,8,-2};
+        System.out.println("Max difference is : "+findMaxDiff(arr, arr.length));
+    }
+
+    public static int findMaxDiff(int arr[], int n){
+
+        int res = arr[1] - arr[0];
+        int minValue = arr[0];
+
+        for(int i = 1; i<n; i++){
+            res = Math.max (res, arr[i] - minValue);
+            minValue = Math.min(minValue, arr[i]);
+        }
+
+
+        return res;
+    }
+    
+}
