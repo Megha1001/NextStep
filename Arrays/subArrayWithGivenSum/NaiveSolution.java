534201eac5a98c759fcb78d3f157478e038ca78c
Megha Verma <meghaverma1001@gmail.com>
2025-12-13 20:52:34 +0530

    Create NaiveSolution.java

---
+++ Arrays/subArrayWithGivenSum/NaiveSolution.java
@@ -0,0 +1,30 @@
+package Arrays.subArrayWithGivenSum;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        // int arr[] = {1,4,20,3,10,5};
+        int arr[] = {1,4,0,0,3,10,5};
+        int sum = 7;
+        System.out.println("is subarray exists with given sum : "+isSubArrayExista(arr, arr.length, sum));
+    }
+
+    public static String isSubArrayExista(int arr[], int n, int sum){
+
+        for(int i=0; i<n; i++){
+            int currSum = 0;
+            for(int j=i; j<n; j++){
+                currSum += arr[j];
+                if(currSum==sum){
+                    return "Yes";
+                }
+                else if(currSum > sum){
+                    break;
+                }
+            }
+        }
+
+        return "No";
+    }
+    
+}
