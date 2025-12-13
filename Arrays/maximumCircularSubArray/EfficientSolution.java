0dde8513eb90b443f5b7987660ab819fde69f813
Megha Verma <meghaverma1001@gmail.com>
2025-12-13 14:29:57 +0530

    Update EfficientSolution.java

--- Arrays/maximumCircularSubArray/EfficientSolution.java
+++ Arrays/maximumCircularSubArray/EfficientSolution.java
@@ -13,5 +13,45 @@ public class EfficientSolution {
         int arr[] = {5, -2, 3, 4};
         System.out.println("Maximum circular subarray sum is : "+findMaxCircularSubArraySum(arr, arr.length));
     }
+
+    public static int findMaxCircularSubArraySum(int arr[], int n){
+        int res = 0;
+        
+        int maxSumOfNormalArray = findMaximumSum(arr,n);
+        int minSumOfNormalArray = findMinimumSum(arr,n);
+
+        res = Math.max(maxSumOfNormalArray,minSumOfNormalArray);
+
+        return res;
+
+    }
+
+    public static int findMaximumSum(int[]arr, int n){
+
+        int res = arr[0];
+        int max_ending = arr[0];
+
+        for(int i=1; i< n; i++){
+            max_ending = Math.max(max_ending + arr[i], arr[i]);
+            res = Math.max(res, max_ending);
+        }
+
+        return res;
+
+    }
+
+    public static int findMinimumSum(int arr[], int n){
+        int res = arr[0];
+        int min_ending = arr[0];
+        int arraySum = arr[0];
+
+        for(int i=1; i<n; i++){
+            min_ending = Math.min(min_ending + arr[i], arr[i]);
+            res = Math.min(res, min_ending);
+            arraySum += arr[i];
+        }
+    
+        return arraySum - res;
+    }
     
 }
