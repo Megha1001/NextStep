8ad44cb2564967329d820525a11c765242e140f6
Megha Verma <meghaverma1001@gmail.com>
2025-12-13 16:41:38 +0530

    Create NaiveSolution.java

---
+++ Arrays/majorityElement/NaiveSolution.java
@@ -0,0 +1,32 @@
+package Arrays.majorityElement;
+
+/*
+ * Majority Element : Its an element that appears more than n/2 times.
+ */
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {8,3,4,8,8};
+        System.out.println("The index of majority of element appears in given array is : "+getIndexOfMajorityElement(arr, arr.length));
+    }
+
+    public static int getIndexOfMajorityElement(int arr[], int n){
+        int res = -1;
+
+        for(int i=0; i<n; i++){
+            int currCount = 1;
+            for(int j=i+1; j<n; j++){
+                if(arr[j]==arr[i]){
+                    ++currCount;
+                }
+            }
+            if(currCount > n/2){
+                return i;
+            }
+        }
+
+        return res;
+    }
+    
+}
