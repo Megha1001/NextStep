1697a358fcfc983554a908abf531407c6426ee1c
Megha Verma <meghaverma1001@gmail.com>
2025-12-13 12:06:57 +0530

    Create NaiveSolution.java

---
+++ Arrays/maximumCircularSubArray/NaiveSolution.java
@@ -0,0 +1,35 @@
+package Arrays.maximumCircularSubArray;
+
+/*
+ * Idea : Consider every element to be start of maximum circular subarray 
+ * Starting with every element there can be N subarray 
+ */
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {5, -2, 3, 4};
+        System.out.println("Maximum circular subarray sum is : "+findMaxCircularSubArraySum(arr, arr.length));
+    }
+
+    public static int findMaxCircularSubArraySum(int arr[], int n){
+        int res = arr[0];
+
+        for(int i=0; i<n; i++){
+            int currSum = arr[i];
+            int currMax = arr[i]; // this is one out of n possible subarray
+
+            for(int j=1; j<n; j++){ //these are n-1 subarrray
+                int idx = (i+j)%n;
+                currSum += arr[idx];
+                currMax = Math.max(currMax, currSum);
+            }
+
+            res = Math.max(res, currMax);
+        }
+
+        return res;
+    }
+
+
+}
