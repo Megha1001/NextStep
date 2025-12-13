b406cbe31696fc2e96a2bef6f14afae2ccfa3b10
Megha Verma <meghaverma1001@gmail.com>
2025-12-13 21:32:36 +0530

    Create EfficientSolutionUsingSlidingWindow.java

---
+++ Arrays/subArrayWithGivenSum/EfficientSolutionUsingSlidingWindow.java
@@ -0,0 +1,34 @@
+package Arrays.subArrayWithGivenSum;
+
+public class EfficientSolutionUsingSlidingWindow {
+
+    public static void main(String args[]){
+        // int arr[] = {1,4,20,3,10,5};
+        int arr[] = {1,4,0,0,3,10,5};
+        int sum = 7;
+        System.out.println("is subarray exists with given sum : "+isSubArrayExista(arr, arr.length, sum));
+    }
+
+
+    public static boolean isSubArrayExista(int arr[], int n, int sum){
+        int start = 0;
+        int currSum = 0;
+
+        for(int i=0; i<n;i++){
+            currSum += arr[i];
+
+            while(currSum > sum){
+                currSum -= arr[start];
+                ++start;
+            }
+
+            if(currSum==sum){
+                return true;
+            }
+
+        }
+
+        return false;
+    }        
+    
+}
