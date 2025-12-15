c33f4b65d518819e637f9afa748ddac2a3fe6b2e
Megha Verma <meghaverma1001@gmail.com>
2025-12-15 21:54:23 +0530

    Create EfficientApproachUsingTwoPointer.java

---
+++ sorting/tripletInSortedArray/EfficientApproachUsingTwoPointer.java
@@ -0,0 +1,14 @@
+package sorting.tripletInSortedArray;
+
+/*
+ * IDEA : if X+Y+Z = K
+ *  => X+Y = K-Z
+ *  for every element call findPair(arr, K-Z)
+ * 
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : O(1)
+ */
+
+public class EfficientApproachUsingTwoPointer {
+    
+}
