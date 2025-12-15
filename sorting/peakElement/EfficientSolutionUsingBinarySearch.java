cd635eb52bbfafcd9ae2238cf73b0981a7764775
Megha Verma <meghaverma1001@gmail.com>
2025-12-15 20:50:22 +0530

    Create EfficientSolutionUsingBinarySearch.java

---
+++ sorting/peakElement/EfficientSolutionUsingBinarySearch.java
@@ -0,0 +1,33 @@
+package sorting.peakElement;
+
+public class EfficientSolutionUsingBinarySearch {
+
+    public static void main(String args[]){
+        int arr [] = {5,20,40,30,20,50,60};
+        System.out.println("The Peak element in given array is : "+findPeak(arr, 0, arr.length-1));
+    }
+
+    public static int findPeak(int arr[], int low, int high){
+
+        while(low <= high){
+
+            int m = low + (high - low)/2;
+
+            if((m==0 || arr[m] >= arr[m-1]) && (arr[m] >= arr[m+1] || m==arr.length-1)){
+                return m;
+            }
+
+            if (m > 0 && arr[m-1] >= arr[m]){
+                high = m-1; //go to left
+            }
+            else {
+                low = m+1; //go to right
+            }
+
+        }
+
+
+        return -1;
+    }
+    
+}
