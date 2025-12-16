e3beaecd0930d30529426ba13f987699b3e952c2
Megha Verma <meghaverma1001@gmail.com>
2025-12-16 22:35:19 +0530

    Create MostEfficientSolutionUsingFindingCycleInArrayForHavingElementsFromZero.java

---
+++ sorting/repeatingElement/MostEfficientSolutionUsingFindingCycleInArrayForHavingElementsFromZero.java
@@ -0,0 +1,35 @@
+package sorting.repeatingElement;
+
+/*
+ * Condition : Dont modify the array and dont use any extra space
+ * 
+ * TIME COMPLEXITY : O(N)
+ * AUXLIARY SPACE : O(1)
+ */
+
+public class MostEfficientSolutionUsingFindingCycleInArrayForHavingElementsFromZero {
+
+    public static void main(String args[]){
+        int arr[] = {0,2,1,3,5,4,6,2};
+        System.out.println("The repeating element in the given array is "+findRepeatingElement(arr));
+    }
+
+    public static int findRepeatingElement(int arr[]){
+        int slow = arr[0] + 1;
+        int fast = arr[0] + 1;
+
+        do{
+            slow = arr[slow] + 1;
+            fast = arr[arr[fast]+1] + 1;
+        }while(slow != fast);
+
+        slow = arr[0]+1;
+        while(slow != fast){
+            slow = arr[slow] + 1;
+            fast = arr[fast] + 1;
+        }
+
+        return slow -1;
+    }
+    
+}
