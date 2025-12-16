f6c2cf9078800fa0d2f75c7c82d77351293f4eb4
Megha Verma <meghaverma1001@gmail.com>
2025-12-16 21:04:10 +0530

    Create EfficientSolution.java

---
+++ sorting/repeatingElement/EfficientSolution.java
@@ -0,0 +1,30 @@
+package sorting.repeatingElement;
+
+/*
+ * We can't modify the array
+ * 
+ * 
+ */
+
+public class EfficientSolution {
+
+    public static void main(String args[]){
+        int arr[] = {0,2,1,3,2,2};
+        System.out.println("Repeating element in given array is : "+findRepeatingElement(arr, arr.length));
+    }
+
+    public static int findRepeatingElement(int arr[], int n){
+
+        boolean visited[] = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            if(visited[arr[i]]){
+                return arr[i];
+            }
+            visited[arr[i]] = true;
+        }
+        
+        return -1;
+    }
+    
+}
