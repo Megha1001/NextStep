5df7f40884b2062cc664a4c5ce6065633def6238
Megha Verma <meghaverma1001@gmail.com>
2025-12-17 22:48:28 +0530

    Create NaiveSolution.java

---
+++ sorting/bubbleSort/NaiveSolution.java
@@ -0,0 +1,37 @@
+package sorting.bubbleSort;
+
+/*
+ * After every pass the maximum element present in the array reaches to final position.
+ */
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {2,10,8,7};
+        System.out.println("Array after sorting using bubble sort is  : ");
+        sortUsingBubbleSort(arr, arr.length);
+    }
+    
+
+    public static void sortUsingBubbleSort(int arr[], int n){
+
+        for(int i=0; i<n-i; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+        }
+
+        //print
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] +" ");
+        }
+
+    }
+
+}
