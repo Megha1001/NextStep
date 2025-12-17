d6aa330c9aeb982d0a5243ee90f80677c77bfc5f
Megha Verma <meghaverma1001@gmail.com>
2025-12-17 23:35:47 +0530

    Create SelectionSortAlgorithm.java

---
+++ sorting/selectionSort/SelectionSortAlgorithm.java
@@ -0,0 +1,37 @@
+package sorting.selectionSort;
+
+public class SelectionSortAlgorithm {
+
+    public static void main(String args[]){
+        int arr[] = {10,5,8,20,2,18};
+        System.out.println("Array after sorting using selection sort is : ");
+        sortUsingSelectionSort(arr, arr.length);
+    }
+
+    public static void sortUsingSelectionSort(int arr[], int n){
+
+        //find min elment in every traversal from remaining considerable array and swap with arr[i]
+        for(int i=0; i<n; i++){
+            int minIdx = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[minIdx]){
+                    minIdx = j;
+                }
+            }
+
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+
+        }
+
+
+        //print
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    
+}
