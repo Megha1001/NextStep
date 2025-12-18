f34943cf77d5fec622881ca54703587187932456
Megha Verma <meghaverma1001@gmail.com>
2025-12-18 14:00:43 +0530

    Update InsertionSortAlgorithm.java

--- sorting/insertionSort/InsertionSortAlgorithm.java
+++ sorting/insertionSort/InsertionSortAlgorithm.java
@@ -14,7 +14,10 @@ public class InsertionSortAlgorithm {
 
             for(int i=0; i<n-1; i++){
                 for(int j=i+1; j>0; j--){
-                    if(arr[j] < arr[j-1]){
+                    if(arr[j] > arr[j-1]){
+                        continue;
+                    }
+                    else if(arr[j] < arr[j-1]){
                         //swap 
                         int temp = arr[j];
                         arr[j] = arr[j-1];
