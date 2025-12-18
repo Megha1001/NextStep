7a915380b5065c0e0bbccd76a46b90885abd9f2f
Megha Verma <meghaverma1001@gmail.com>
2025-12-18 14:10:56 +0530

    Update InsertionSortBookVersion.java

--- sorting/insertionSort/InsertionSortBookVersion.java
+++ sorting/insertionSort/InsertionSortBookVersion.java
@@ -18,6 +18,10 @@ public class InsertionSortBookVersion {
         for(int i=1; i<n; i++){
             int key = arr[i];
             int j = i-1;
+
+            if(arr[j] < key){
+                continue;
+            }
             
             while(j>=0 && arr[j] > key){
                 arr[j+1] = arr[j];
