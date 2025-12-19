c9ff4d4c3b13821b67043e0a5346bd02e3b61b36
Megha Verma <meghaverma1001@gmail.com>
2025-12-19 15:07:50 +0530

    Create NaiveSolutionUsingMergeFunction.java

---
+++ sorting/unionOfTwoSortedArrays/NaiveSolutionUsingMergeFunction.java
@@ -0,0 +1,62 @@
+package sorting.unionOfTwoSortedArrays;
+
+public class NaiveSolutionUsingMergeFunction {
+
+    public static void main(String args[]){
+        // int a1 [] = {3,5,8};
+        // int a2[] = {2,8,9,10,15};
+        int a1 [] = {3,3,3};
+        int a2[] = {3,3,4,4,4};
+
+        System.out.println("Union of given arrays is : ");
+        printUnion(a1, a2, a1.length, a2.length);
+    }
+
+    public static void printUnion(int a1[], int a2[], int n1, int n2){
+
+        int i = 0, j = 0;
+    
+        while(i < n1 && j < n2){
+    
+            if(i > 0 && a1[i] == a1[i-1]){
+                i++;
+                continue;
+            }
+    
+            if(j > 0 && a2[j] == a2[j-1]){
+                j++;
+                continue;
+            }
+    
+            if(a1[i] < a2[j]){
+                System.out.print(a1[i] + " ");
+                i++;
+            }
+            else if(a1[i] > a2[j]){
+                System.out.print(a2[j] + " ");
+                j++;
+            }
+            else {
+                System.out.print(a1[i] + " ");
+                i++;
+                j++;
+            }
+        }
+    
+        while(i < n1){
+            if(i == 0 || a1[i] != a1[i-1]){
+                System.out.print(a1[i] + " ");
+            }
+            i++;
+        }
+    
+        while(j < n2){
+            if(j == 0 || a2[j] != a2[j-1]){
+                System.out.print(a2[j] + " ");
+            }
+            j++;
+        }
+    }
+    
+    
+}
