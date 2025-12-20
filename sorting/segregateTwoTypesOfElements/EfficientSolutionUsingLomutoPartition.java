187f2c736d9455b68330eefe4eee03501a7f81dd
Megha Verma <meghaverma1001@gmail.com>
2025-12-20 17:54:12 +0530

    transition method to correct class

--- sorting/segregateTwoTypesOfElements/EfficientSolutionUsingLomutoPartition.java
+++ sorting/segregateTwoTypesOfElements/EfficientSolutionUsingLomutoPartition.java
@@ -13,29 +13,23 @@ public class EfficientSolutionUsingLomutoPartition {
         segregateNegAndPos(arr, arr.length);
     }
 
-    public static void segregateNegAndPos(int arr[], int n){
-        int temp [] = new int[n];
-        int j=0;
-
-        //copy -ve
-        for(int i=0; i<n ; i++){
-            if(arr[i] < 0){
-                temp[j++] = arr[i];
+        //use partition function of quicksort
+        public static void segregateNegAndPos(int arr[], int n){
+            int i = -1;
+            for(int j=0; j<n; j++){
+                if(arr[j] < 0){
+                    ++i;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+    
+            //print 
+            for(int k=0; k<n; k++){
+                System.out.print(arr[k] + " ");
             }
         }
-
-        //copy +ve
-        for(int i=0; i<n;i++){
-            if(arr[i] >= 0){
-                temp[j++] = arr[i];
-            }
-        }
-
-        //copy to original array and print
-        for(int i=0; i<n; i++){
-            arr[i] = temp[i];
-            System.out.print(arr[i] + " ");
-        }
-    }
+    
     
 }
