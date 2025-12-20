2cd1ae0444d31311254a5f9cef3d97067226175d
Megha Verma <meghaverma1001@gmail.com>
2025-12-20 17:47:58 +0530

    Create NaiveSolution.java

---
+++ sorting/segregateTwoTypesOfElements/NaiveSolution.java
@@ -0,0 +1,30 @@
+package sorting.segregateTwoTypesOfElements;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {6,-5,4,1,-1,7,8};
+        System.out.println("Array after segregating -ve and +ve is : ");
+        segregateNegAndPos(arr, arr.length);
+    }
+
+
+    //use partition function of quicksort
+    public static void segregateNegAndPos(int arr[], int n){
+        int i = -1;
+        for(int j=0; j<n; j++){
+            if(arr[j] < 0){
+                ++i;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        //print 
+        for(int k=0; k<n; k++){
+            System.out.print(arr[k] + " ");
+        }
+    }
+    
+}
