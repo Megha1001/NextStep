b424a32fb733d4c9913aa6dba4c30af7c279cbbf
Megha Verma <meghaverma1001@gmail.com>
2025-12-20 17:56:38 +0530

    Update EfficientSolutionUsingHoaresPartition.java

--- sorting/segregateTwoTypesOfElements/EfficientSolutionUsingHoaresPartition.java
+++ sorting/segregateTwoTypesOfElements/EfficientSolutionUsingHoaresPartition.java
@@ -5,37 +5,40 @@ package sorting.segregateTwoTypesOfElements;
  * AUXILIARY SPACE : O(N)
  */
 
-public class EfficientSolutionUsingLomutoPartition {
+public class EfficientSolutionUsingHoaresPartition {
 
     public static void main(String args[]){
         int arr[] = {6,-5,4,1,-1,7,8};
         System.out.println("Array after segregating -ve and +ve is : ");
         segregateNegAndPos(arr, arr.length);
+
+        //print
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] +" ");
+        }
+
     }
 
     public static void segregateNegAndPos(int arr[], int n){
-        int temp [] = new int[n];
-        int j=0;
+        int i=-1;
+        int j = n;
 
-        //copy -ve
-        for(int i=0; i<n ; i++){
-            if(arr[i] < 0){
-                temp[j++] = arr[i];
-            }
+        do{
+            ++i;
+        }while(arr[i] < 0);
+
+
+        do{
+            --j;
+        }while(arr[j] >=0);
+
+        if(i>=j){
+            return;
         }
 
-        //copy +ve
-        for(int i=0; i<n;i++){
-            if(arr[i] >= 0){
-                temp[j++] = arr[i];
-            }
-        }
-
-        //copy to original array and print
-        for(int i=0; i<n; i++){
-            arr[i] = temp[i];
-            System.out.print(arr[i] + " ");
-        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
 }
