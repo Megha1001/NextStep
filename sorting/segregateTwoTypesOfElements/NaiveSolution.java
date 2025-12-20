f4c57141804614a72f9762820e68c4d1a12e9c68
Megha Verma <meghaverma1001@gmail.com>
2025-12-20 17:48:36 +0530

    Update NaiveSolution.java

--- sorting/segregateTwoTypesOfElements/NaiveSolution.java
+++ sorting/segregateTwoTypesOfElements/NaiveSolution.java
@@ -1,5 +1,10 @@
 package sorting.segregateTwoTypesOfElements;
 
+/*
+ * TIME COMPLEXITY : Theta(N)
+ * AUXILIARY SPACE : O(1)
+ */
+
 public class NaiveSolution {
 
     public static void main(String args[]){
