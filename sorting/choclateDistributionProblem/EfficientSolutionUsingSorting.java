6d5a518f818eb801f039314fd8d87f57c41277f9
Megha Verma <meghaverma1001@gmail.com>
2025-12-20 16:23:51 +0530

    Create EfficientSolutionUsingSorting.java

---
+++ sorting/choclateDistributionProblem/EfficientSolutionUsingSorting.java
@@ -0,0 +1,25 @@
+package sorting.choclateDistributionProblem;
+
+import java.util.Arrays;
+
+public class EfficientSolutionUsingSorting {
+
+    public static void main(String args[]){
+        int arr[] = {7,3,2,4,9,12,56};
+        int k = 3;
+        System.out.println("The minimum difference between the max and min choclate count is : "+findMinDiff(arr, arr.length, k));
+    }
+
+    public static int findMinDiff(int arr[], int n, int k){
+        int res = Integer.MAX_VALUE;
+
+        Arrays.sort(arr);
+
+        for(int i=0; i<n-k; i++){
+            res = Math.min(res, Math.abs(arr[i]-arr[i+k-1]));
+        }
+
+        return res;
+    }
+    
+}
