1998732d4e1383fbf84648442747edb0e76a7d5e
Megha Verma <meghaverma1001@gmail.com>
2025-12-22 21:07:11 +0530

    Update LinearProbingImpl.java

--- hashing/openAddressing/LinearProbingImpl.java
+++ hashing/openAddressing/LinearProbingImpl.java
@@ -2,15 +2,16 @@ package hashing.openAddressing;
 
 public class LinearProbingImpl {
 
-    class MyHash{
+    static class MyHash{
 
         int cap; // what is the limit
         int size; //how many occupied
         int arr[];
 
-        MyHash(int c){
+        public MyHash(int c){
             this.cap = c;
             this.size = 0;
+            this.arr = new int[cap];
 
             for(int i=0; i<cap; i++){
                 arr[i] = -1; // all are empty
@@ -63,6 +64,46 @@ public class LinearProbingImpl {
             return false;
         }
 
+        public boolean erase(int key){
+            if(size==0){
+                return false; //arr is empty
+            }
+
+            int h = hash(key);
+            int i = h;
+            
+            while(arr[i] !=-1){
+                if(arr[i] == key){
+                    arr[i] = -2;
+                    --size;
+                    return true;
+                }
+                i= (i+1)%cap;
+                if(i==h){
+                    return false;
+                }
+            }
+
+            return false;
+
+        }
+
     }
     
+
+    public static void main(String args[]){
+        MyHash mh = new MyHash(7);
+	    mh.insert(49);
+	    mh.insert(56);
+	    mh.insert(72);
+	    if(mh.search(56)==true)
+	        System.out.println("Yes");
+	    else
+	        System.out.println("No");
+	    mh.erase(56);
+	    if(mh.search(56)==true)
+	        System.out.println("Yes");
+	    else
+            System.out.println("No");
+    }
 }
