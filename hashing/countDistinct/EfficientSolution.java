ee1b7269e5e83df94ae3b557ced55f237e6cb80f
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 08:40:03 +0530

    Update EfficientSolution.java

--- hashing/countDistinct/EfficientSolution.java
+++ hashing/countDistinct/EfficientSolution.java
@@ -1,15 +1,25 @@
 package hashing.countDistinct;
 
 import java.util.HashSet;
+import java.util.Arrays;
+
+/*
+ * TIME COMPLEXITY : Theta(N)
+ * AUXIliARY SPACE : O(1)
+ */
 
 public class EfficientSolution {
 
     public static void main(String args[]){
-        int arr[] = {10,20,10,20,30};
+        Integer arr[] = {10,20,10,20,30};
         System.out.println("Number of distinct elements in given array is : "+findDistinct(arr, arr.length));
+
+        //another way
+        HashSet<Integer> h = new HashSet<>(Arrays.asList(arr));
+        System.out.println("Number of distinct elements in given array is : "+h.size());
     }
 
-    public static int findDistinct(int arr[], int n){
+    public static int findDistinct(Integer arr[], int n){
         HashSet<Integer> h = new HashSet<>();
 
         for(int i=0; i<n; i++){
