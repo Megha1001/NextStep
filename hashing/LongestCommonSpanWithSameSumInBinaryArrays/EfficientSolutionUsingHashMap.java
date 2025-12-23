09fca268fb31df0716d9a2f33c0525a3261aadfe
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 20:02:39 +0530

    Update EfficientSolutionUsingHashMap.java

--- hashing/LongestCommonSpanWithSameSumInBinaryArrays/EfficientSolutionUsingHashMap.java
+++ hashing/LongestCommonSpanWithSameSumInBinaryArrays/EfficientSolutionUsingHashMap.java
@@ -15,6 +15,57 @@ package hashing.LongestCommonSpanWithSameSumInBinaryArrays;
  * 2nd and third will balance each other in case of common subarray. Hence, we need to find subarray with sum = 0
  */
 
+import java.util.HashMap;
+
 public class EfficientSolutionUsingHashMap {
+    
+    public static void main(String args[]){
+        int a1[] = {0,1,0,0,0,0};
+        int a2[] = {1,0,1,0,0,1};
+
+        System.out.println("The length of longest common subarray with same sum and same start/end idx is : "+findLongestCommonSubArray(a1, a2, a1.length));
+    }
+
+    public static int findLongestCommonSubArray(int a1[], int a2[], int n){
+
+        //compute difference array
+        int temp [] = new int[n];
+        for(int i=0; i<n; i++){
+            temp[i] = a1[i] - a2[i];
+        }
+
+        return findLongestSubArrayWithSumZero(temp, n, 0);
+
+    }
+
+    public static int findLongestSubArrayWithSumZero(int arr[], int n, int sum){
+        int res = 0;
+
+        int prefix_sum = 0;
+        int idx = -1;
+        HashMap<Integer, Integer> h = new HashMap<>();
+        
+    
+        for(int i=0; i<n; i++){
+            prefix_sum += arr[i];
+
+
+            if(prefix_sum == sum){
+                res = Math.max(res, i+1);
+            }
+
+            if(h.containsKey(prefix_sum - sum)){
+                ++idx;
+                res = Math.max(res, idx - h.get(prefix_sum - sum));
+            }else {
+                h.put(prefix_sum, ++idx);
+            }
+
+
+           
+        }
+
+        return res;
+    }
     
 }
