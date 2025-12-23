0436f8e3b877033ae27e2bfd788b1282a90304f1
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 21:11:39 +0530

    Update NaiveSolution.java

--- hashing/longestConsecutiveSequence/NaiveSolution.java
+++ hashing/longestConsecutiveSequence/NaiveSolution.java
@@ -13,7 +13,8 @@ public class NaiveSolution {
         // int arr[] = {7,8,5,10,9};
         // int arr[] = {1,9,3,4,2,20};
         // int arr[] = {8,20,7,30};
-        int arr[] = {20,30,40};
+        // int arr[] = {20,30,40};
+        int arr[] = {1, 2, 2, 3}; // IMP Input , we should ignore duplicate 2 and not reset the counter
         System.out.println("The longest consecutive sequence in given array is : "+findLongestConsequtiveSubSeqLength(arr, arr.length));
     }
 
@@ -28,7 +29,16 @@ public class NaiveSolution {
             if(arr[i] - arr[i-1] ==1){
                 ++count;
             }
-            else {
+            else if (arr[i]!=arr[i-1]){ // to ignore element in cases like : {2,3,3} , we want to ignore next 3
+                /*
+                 * | i | prev | curr | diff | count     | res | Explanation                 |
+                   | - | ---- | ---- | ---- | --------- | --- | --------------------------- |
+                   | 1 | 2    | 3    | 1    | 2         | 1   | consecutive                 |
+                   | 2 | 3    | 3    | 0    | reset → 1 | 2   | ❌ duplicate breaks sequence |
+                   | 3 | 3    | 4    | 1    | 2         | 2   | restarts                    |
+                    final answer should be 3 not 2
+                    2, 3, 4  → length = 3
+                 */
                 res = Math.max(res, count);
                 count = 1;
             }
