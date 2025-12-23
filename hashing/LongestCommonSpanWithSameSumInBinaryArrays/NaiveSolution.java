0ae3c4616a0d656004b8981dcb500a002cdd2a69
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 19:10:46 +0530

    Update NaiveSolution.java

--- hashing/LongestCommonSpanWithSameSumInBinaryArrays/NaiveSolution.java
+++ hashing/LongestCommonSpanWithSameSumInBinaryArrays/NaiveSolution.java
@@ -13,5 +13,31 @@ package hashing.LongestCommonSpanWithSameSumInBinaryArrays;
  */
 
 public class NaiveSolution {
-    
+
+    public static void main(String args[]){
+        int a1[] = {0,1,0,0,0,0};
+        int a2[] = {1,0,1,0,0,1};
+
+        System.out.println("The length of longest common subarray with same sum and same start/end idx is : "+findLongestCommonSubArray(a1, a2, a1.length));
+    }    
+
+    public static int findLongestCommonSubArray(int a1[], int a2[], int n){
+
+        int res = 0;
+
+        for(int i=0; i<n; i++){
+            int sum1=0, sum2=0;
+            for(int j=i; j<n; j++){
+                sum1 += a1[j];
+                sum2 += a2[j];
+
+                if(sum1 == sum2){
+                    res = Math.max(res, j-i+1);
+                }
+            }
+        }
+
+        return res;
+
+    }
 }
