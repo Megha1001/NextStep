a5b1fc8cca911f86bb313ec90605e7f87cd12e06
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 12:32:19 +0530

    Create EfficientSolutionUsingHashSet.java

---
+++ hashing/pairWithGivenSumInUnsortedArray/EfficientSolutionUsingHashSet.java
@@ -0,0 +1,32 @@
+package hashing.pairWithGivenSumInUnsortedArray;
+
+import java.util.HashSet;
+
+/*
+ * WE CAN'T INSERT ALL THEN TRAVERSE otherwise for this example     int arr[] = {8,3,2,5}; int sum = 6; it will give true
+ */
+
+public class EfficientSolutionUsingHashSet {
+
+    public static void main(String args[]){
+        // int arr[] = {8,3,4,2,5};
+        int arr[] = {8,3,2,5};
+        int sum = 6;
+        System.out.println("is the pair exist in given array with given sum : ? "+isPair(arr, arr.length, sum));
+    }
+
+    public static boolean isPair(int arr[], int n, int sum){
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int x : arr){
+            if(set.contains(sum-x)){
+                return true;
+            }else{
+                set.add(x);
+            }
+        }
+
+        return false;
+    }
+    
+}
