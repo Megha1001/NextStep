d654eca8b09f5fd83739b449ce35e11be9dece8e
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 21:54:42 +0530

    Create EfficientSolutionUsingHashing.java

---
+++ hashing/longestConsecutiveSequence/EfficientSolutionUsingHashing.java
@@ -0,0 +1,14 @@
+package hashing.longestConsecutiveSequence;
+
+/*
+ * IDEA : 
+ * 1. Store the element in HashSet -> distinct
+ * 2. Traverse Hashset
+ *  if (h.contains(x-1)) -> that x is not the start of subsequence
+ *  if (!h.contains(x-1)) -> that x is start of subsequence
+ *      then check for x+1, x+2 and so on
+ */
+
+public class EfficientSolutionUsingHashing {
+    
+}
