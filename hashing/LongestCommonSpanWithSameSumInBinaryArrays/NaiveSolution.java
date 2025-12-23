0f4a45939844c5f8483c86fd4cecdc1331d2f7e7
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 19:01:28 +0530

    Create NaiveSolution.java

---
+++ hashing/LongestCommonSpanWithSameSumInBinaryArrays/NaiveSolution.java
@@ -0,0 +1,17 @@
+package hashing.LongestCommonSpanWithSameSumInBinaryArrays;
+/*
+ * Problem : find the longest common subarray b/w two array with same sum and same starting and ending index
+ * 
+ * For Example : 
+ * a1 : {0,1,0,0,0,0}
+ *         -------
+ * a2 : {1,0,1,0,0,1}
+ *         -------
+ * 
+ * The underlined is common. Hence, ans = 4
+ * 
+ */
+
+public class NaiveSolution {
+    
+}
