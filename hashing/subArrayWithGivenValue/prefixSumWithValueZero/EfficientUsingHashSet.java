6435cb0b0d4ac728409aec202ebf533d3ab4bf29
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 13:36:17 +0530

    Create EfficientUsingHashSet.java

---
+++ hashing/subArrayWithGivenValue/prefixSumWithValueZero/EfficientUsingHashSet.java
@@ -0,0 +1,39 @@
+package hashing.subArrayWithGivenValue.prefixSumWithValueZero;
+
+import java.util.HashSet;
+
+public class EfficientUsingHashSet {
+
+    public static void main(String args[]){
+        int arr[] = {-3,4,-3,-1,1};
+        int sum = 0;
+
+        System.out.println("is subarray exists with given sum : "+isSubArrayExists(arr, arr.length, sum));
+    }
+
+
+    public static boolean isSubArrayExists(int arr[], int n, int sum){
+        HashSet<Integer> h = new HashSet<>();
+
+        int prefix_sum = 0;
+
+        for(int x : arr){
+            prefix_sum += x;
+
+            if(h.contains(prefix_sum)){
+                return true;
+            }
+
+            if(prefix_sum == sum){
+                return true; // import cases where whole array gives the required sum  [-3,2,1] or even [-3,2,1,4]
+            }
+
+            h.add(prefix_sum);
+
+        }
+
+        return false;
+
+    }
+    
+}
