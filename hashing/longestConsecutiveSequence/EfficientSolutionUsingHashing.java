7f2381f458672eb481961580bec2db4d4f1f52db
Megha Verma <meghaverma1001@gmail.com>
2025-12-23 22:06:29 +0530

    Update EfficientSolutionUsingHashing.java

--- hashing/longestConsecutiveSequence/EfficientSolutionUsingHashing.java
+++ hashing/longestConsecutiveSequence/EfficientSolutionUsingHashing.java
@@ -9,6 +9,43 @@ package hashing.longestConsecutiveSequence;
  *      then check for x+1, x+2 and so on
  */
 
+import java.util.HashSet;
+
 public class EfficientSolutionUsingHashing {
+
+    public static void main(String args[]){
+        int arr[] = {1,3,9,2,8,2};
+        System.out.println("The longest consecutive sequence length is : "+findLongestConsequtiveSubSequence(arr, arr.length));
+    }
+
+
+    public static int findLongestConsequtiveSubSequence(int arr[], int n){
+        int res = 1;
+        HashSet<Integer> h = new HashSet<>();
+
+        //Step-1
+        for(int i : arr){
+            h.add(i);
+        }
+
+        //Step-2
+        for(Integer x : h){
+
+            //start of subsequence
+            if(!h.contains(x-1)){
+                int curr = 1;
+
+                while(h.contains(x+curr)){
+                    ++curr;
+                }
+
+                res = Math.max(res, curr);
+
+            }
+
+        }
+
+        return res;
+    }
     
 }
