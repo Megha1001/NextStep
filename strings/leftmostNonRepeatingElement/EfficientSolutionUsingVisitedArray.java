d7cb7e80022e64e579ace0a47deedb99654f2d12
Megha Verma <meghaverma1001@gmail.com>
2025-12-24 21:29:31 +0530

    Create EfficientSolutionUsingVisitedArray.java

---
+++ strings/leftmostNonRepeatingElement/EfficientSolutionUsingVisitedArray.java
@@ -0,0 +1,28 @@
+package strings.leftmostNonRepeatingElement;
+
+public class EfficientSolutionUsingVisitedArray {
+
+    public static int CHAR = 256;
+
+    public static void main(String args[]){
+        String s = "abbcc";
+        System.out.println("The leftmost non repeating character element in given string is present at idx : "+findLeftMostNonRepeatingCharacter(s, s.length()));
+    }
+    
+    public static int findLeftMostNonRepeatingCharacter(String s, int n){
+        boolean visited[] = new boolean[CHAR];
+
+        int res = -1;
+
+        for(int i=n-1; i>=0; i--){
+            if(!visited[s.charAt(i)]){
+                res = i;
+                visited[s.charAt(i)] = true;
+            }
+        }
+
+        return res;
+
+    }
+
+}
