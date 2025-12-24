db3f03d9489e3ceaa0adcbb5644257433ff06392
Megha Verma <meghaverma1001@gmail.com>
2025-12-24 19:39:29 +0530

    Update EfficientSolutionUsingArrayOf26Size.java

--- strings/anagram/EfficientSolutionUsingArrayOf26Size.java
+++ strings/anagram/EfficientSolutionUsingArrayOf26Size.java
@@ -3,14 +3,14 @@ package strings.anagram;
 public class EfficientSolutionUsingArrayOf26Size {
 
     public static void main(String args[]){
-        // String s1 = "listen";
-        // String s2 = "slient";
-        String s1 = "abdfd";
-        String s2 = "adfda";
+        String s1 = "listen";
+        String s2 = "slient";
+        // String s1 = "abdfd";
+        // String s2 = "adfda";
         int n1 = s1.length();
         int n2 = s2.length();
 
-        System.out.println("are the two strings anagram ? "+isAnagram(s1.toLowerCase(), s2.toLowerCase(), n1, n2));
+        System.out.println("are the two strings anagram ? "+isAnagram(s1, s2, n1, n2));
     }
 
     public static boolean isAnagram(String s1, String s2, int n1, int n2){
@@ -19,17 +19,14 @@ public class EfficientSolutionUsingArrayOf26Size {
             return false;
         }
 
-        int arr[] = new int[26];
+        int arr[] = new int[256];
 
         for(int i=0; i<n1; i++){
-            arr[s1.charAt(i) - 'a']++;
+            arr[s1.charAt(i)]++;
+            arr[s2.charAt(i)]--;
         }
 
-        for(int i=0; i<n2; i++){
-            arr[s2.charAt(i) -'a']--;
-        }
-
-        for(int i=0; i<26; i++){
+        for(int i=0; i<256; i++){
             if(arr[i]!=0){
                 return false;
             }
