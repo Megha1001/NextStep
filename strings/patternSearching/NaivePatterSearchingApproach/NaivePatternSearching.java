3d3ef8c09aa3204b8cbdd0512d936bcbecc8b1b4
Megha Verma <meghaverma1001@gmail.com>
2025-12-25 14:14:34 +0530

    Create NaivePatternSearching.java

---
+++ strings/patternSearching/NaivePatterSearchingApproach/NaivePatternSearching.java
@@ -0,0 +1,32 @@
+package strings.patternSearching.NaivePatterSearchingApproach;
+
+public class NaivePatternSearching {
+    
+    public static void main(String arg[]){
+        String txt = "ABCABCD";
+        String pattern = "ABCD";
+
+        System.out.println("Given pattern is present at index in given txt :- ");
+        findPattern(txt, pattern);
+    }
+
+    public static void findPattern(String txt, String pattern){
+
+        int m = pattern.length();
+        int n = txt.length();
+
+        for(int i=0; i<=n-m ; i++){
+            int j=0;
+            for(; j<m; j++){
+                if(txt.charAt(i+j) != pattern.charAt(j)){
+                    break;
+                }
+            }
+            if(j==m){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+
+}
