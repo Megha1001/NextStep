1dfa9818e26c008aab2809caa51f4346b39b1315
Megha Verma <meghaverma1001@gmail.com>
2025-12-25 15:41:36 +0530

    Create NaivePatternSearchingOptimizedWhenPatternHasDistinctElements.java

---
+++ strings/patternSearching/NaivePatterSearchingApproach/NaivePatternSearchingOptimizedWhenPatternHasDistinctElements.java
@@ -0,0 +1,40 @@
+package strings.patternSearching.NaivePatterSearchingApproach;
+
+public class NaivePatternSearchingOptimizedWhenPatternHasDistinctElements {
+
+    public static void main(String arg[]){
+        String txt = "ABCABCD";
+        String pattern = "ABCD";
+
+        System.out.println("Given pattern is present at index in given txt :- ");
+        findPattern(txt, pattern);
+    }
+
+    public static void findPattern(String txt, String pat){
+        int n = txt.length();
+        int m = pat.length();
+
+        for(int i=0; i<= n-m; ){
+            int j=0;
+            for(; j<m; j++){
+                if(txt.charAt(i+j) != pat.charAt(j)){
+                    break;
+                }
+            }
+
+            if(j==m){
+                System.out.print(i+ " ");
+            }
+
+            //OPTIMIZATION
+            if(j==0){
+                ++i;
+            }
+            else {
+                i = i+j;
+            }
+
+        }
+    }
+    
+}
