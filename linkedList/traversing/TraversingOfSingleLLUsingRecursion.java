6e257ea90f0298de91cfee2b3b8218d0c7d9f514
Megha Verma <meghaverma1001@gmail.com>
2025-12-26 12:03:52 +0530

    Create TraversingOfSingleLLUsingRecursion.java

---
+++ linkedList/traversing/TraversingOfSingleLLUsingRecursion.java
@@ -0,0 +1,35 @@
+package linkedList.traversing;
+
+public class TraversingOfSingleLLUsingRecursion {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+        public static void traverseRecursively(Node head){
+            if(head==null){
+                return ;
+            }
+
+            System.out.print(head.data + " ");
+            traverseRecursively(head.next);
+        }
+        
+    }
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        System.out.println("Element of LL are : ");
+        Node.traverseRecursively(head);
+    }
+    
+}
