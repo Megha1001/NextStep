74226b88b439b41ca533d04028bff6445a3c0ed0
Megha Verma <meghaverma1001@gmail.com>
2025-12-26 19:25:17 +0530

    Create InsertAtBeginDLL.java

---
+++ linkedList/doublyLL/insertion/InsertAtBeginDLL.java
@@ -0,0 +1,51 @@
+package linkedList.doublyLL.insertion;
+
+public class InsertAtBeginDLL {
+
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.prev = head;
+        head.next.next = new Node(30);
+        head.next.next.prev = head.next;
+
+        head = insertAtBegin(head, 5);
+        traverse(head);
+
+    }
+
+    public static Node insertAtBegin(Node head, int x){
+        Node newNode = new Node(x);
+        if(head == null){
+            return newNode;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        return newNode;
+
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            return;
+        }
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+    
+}
