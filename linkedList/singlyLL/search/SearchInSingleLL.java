e012ab1fbba0b6f16e7f24b603718f214ced2e1a
Megha Verma <meghaverma1001@gmail.com>
2025-12-26 17:33:59 +0530

    Searching in LL

---
+++ linkedList/singlyLL/search/SearchInSingleLL.java
@@ -0,0 +1,45 @@
+package linkedList.singlyLL.search;
+
+class Node{
+
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
+public class SearchInSingleLL {
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(5);
+        head.next.next = new Node(20);
+        head.next.next.next = new Node(15);
+
+        System.out.println("Element is present at idx : "+ search(head, 20));
+    }
+
+    public static int search(Node head, int x){
+        if(head == null){
+            return -1;
+        }
+
+        Node curr = head;
+        int idx = 0;
+        while(curr != null){
+            ++idx;
+            if(curr.data == x){
+                return idx;
+            }
+            curr = curr.next;
+        }
+
+        return -1;
+
+    }
+    
+}
