dd73dd3d94fa90f18d6a827fe68c8a86c3aae3e1
Megha Verma <meghaverma1001@gmail.com>
2025-12-27 14:12:51 +0530

    Create DeleteKthNodeCLL.java

---
+++ linkedList/circularLL/deletion/DeleteKthNodeCLL.java
@@ -0,0 +1,68 @@
+package linkedList.circularLL.deletion;
+
+public class DeleteKthNodeCLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = head;
+
+        traverse(head);
+
+        int k = 2;
+        // head = deleteNodeAtKthPosition(head, 1);
+        // head = deleteNodeAtKthPosition(head, 3);
+        System.out.println();
+        traverse(head);
+
+    }
+
+    public static Node deleteNodeAtKthPosition(Node head, int k){
+        if(head == null){
+            return null;
+        }
+
+        //k=1, delete head
+        if(k==1){
+            head.data = head.next.data;
+            head.next = head.next.next;
+            return head;
+        }
+
+        Node curr = head;
+        // traverse till k-1
+        int count = 1;
+
+        while(count != k-1){
+            curr = curr.next;
+            ++count;
+        }
+
+        curr.next = curr.next.next;
+        return head;
+
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node curr = head;
+        do{
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }while(curr != head);
+    }
+    
+}
