9f8e41e4689d0de4b65a2d2d50e4cbb049c3d73f
Megha Verma <meghaverma1001@gmail.com>
2025-12-27 11:57:14 +0530

    Create CreateCLL.java

---
+++ linkedList/circularLL/creation/CreateCLL.java
@@ -0,0 +1,47 @@
+package linkedList.circularLL.creation;
+
+/*
+ * TIME COMPLEXITY : Theta(N)
+ * AUXILIARY SPACE : O(1)
+ */
+
+public class CreateDLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = head;
+
+
+        traverse(head);
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node curr = head;
+        while(curr.next != head){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+
+        //for last node
+        System.out.print(curr.data + " ");
+
+    }
+
+}
