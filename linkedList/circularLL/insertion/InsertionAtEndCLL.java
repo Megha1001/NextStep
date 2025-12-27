401053dce236f2a1c7f7222c21cb00581b6fc2a5
Megha Verma <meghaverma1001@gmail.com>
2025-12-27 13:04:50 +0530

    Create InsertionAtEndCLL.java

---
+++ linkedList/circularLL/insertion/InsertionAtEndCLL.java
@@ -0,0 +1,58 @@
+package linkedList.circularLL.insertion;
+
+public class InsertionAtEndCLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = insertAtEnd(null, 10);
+        head = insertAtEnd(head, 20);
+        head = insertAtEnd(head, 30);
+        head = insertAtEnd(head, 40);
+        head = insertAtEnd(head, 50);
+
+        traverse(head);
+    }
+
+    public static Node insertAtEnd(Node head, int x){
+        Node newNode = new Node(x);
+        if(head == null){
+            newNode.next = newNode;
+            return newNode;
+        }
+
+        Node curr = head;
+
+        //go to last node
+        while(curr.next != head){
+            curr = curr.next;
+        }
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+
+        //dont change head
+        return head;
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            return;
+        }
+
+        //handle head seperately
+        System.out.print(head.data + " ");
+
+        for(Node curr = head.next; curr != head; curr = curr.next){
+            System.out.print(curr.data + " ");
+        }
+    }
+    
+}
