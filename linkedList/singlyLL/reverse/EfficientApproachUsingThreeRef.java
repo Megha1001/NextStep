6858c789ec01c35df221043a91c5d4a043ec316d
Megha Verma <meghaverma1001@gmail.com>
2025-12-28 09:39:49 +0530

    Create EfficientApproachUsingThreeRef.java

---
+++ linkedList/singlyLL/reverse/EfficientApproachUsingThreeRef.java
@@ -0,0 +1,56 @@
+package linkedList.singlyLL.reverse;
+
+public class EfficientApproachUsingThreeRef {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        System.out.println("Reverse of given LL is : ");
+        head = reverse(head);
+        traverse(head);
+    }
+
+
+    public static Node reverse(Node head){
+
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        Node prev = null;
+        Node curr = head;
+
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void traverse(Node head){
+        if(head==null){
+            return;
+        }
+
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+}
