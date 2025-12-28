612235852aa195c75e29590d5cd415aa03c8bd20
Megha Verma <meghaverma1001@gmail.com>
2025-12-28 21:43:54 +0530

    Create EfficientMethodUsingFLoydAlgo.java

---
+++ linkedList/loopDetectionAndRemoval/EfficientMethodUsingFLoydAlgo.java
@@ -0,0 +1,83 @@
+package linkedList.loopDetectionAndRemoval;
+
+public class EfficientMethodUsingFLoydAlgo {
+    
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(2);
+        head.next.next.next = head;
+
+        System.out.println("LL after loop removal if present is  :  ");
+
+        loopDetectionAndRemoval(head);
+        traverse(head);
+    }
+
+    public static void loopDetectionAndRemoval(Node head){
+        if(head == null){
+            return;
+        }
+
+        //loop detection
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+        //no loop
+        if(slow != fast){
+            return;
+        }
+
+        //loop removal
+        slow = head;
+
+        //finding meeting point --> when not meet at head
+        while(slow.next != fast.next){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // Step 3: Find last node of loop --> when meet at head
+        while(fast.next != slow){
+            fast = fast.next;
+        }
+
+        // Step 4: Remove loop
+        fast.next = null;
+
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+    
+
+    
+}
