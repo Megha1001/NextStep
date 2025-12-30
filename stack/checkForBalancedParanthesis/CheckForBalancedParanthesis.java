be43c3c23afcf7e3267baa4d3199ed920f632d8d
Megha Verma <meghaverma1001@gmail.com>
2025-12-30 15:22:10 +0530

    Create CheckForBalancedParanthesis.java

---
+++ stack/checkForBalancedParanthesis/CheckForBalancedParanthesis.java
@@ -0,0 +1,49 @@
+package stack.checkForBalancedParanthesis;
+
+import java.util.ArrayDeque;
+
+public class CheckForBalancedParanthesis {
+
+    public static void main(String args[]){
+        String input = "{[({})]}";
+        System.out.println("are the parenthesis balanced ? "+isBalanced(input));
+    }
+
+    public static boolean isBalanced(String input){
+        char [] c = input.toCharArray();
+        ArrayDeque<Character> s = new ArrayDeque<>();
+
+        for(char character : c){
+
+            switch(character) {
+                case '{' :                
+                case '[' :
+                case '(' :
+                    s.push(character);
+                    break;
+                
+                case '}' : 
+                case ']' : 
+                case ')' : 
+                    if(s.size()==0){
+                        return false;
+                    }
+                    char lastCharacter = s.pop();
+                    boolean matched = (lastCharacter == '{' && character == '}')||
+                    (lastCharacter == '[' && character == ']')||
+                    (lastCharacter == '(' && character == ')');
+
+                    if(!matched){
+                        return false;
+                    }
+                    break;
+                
+            }
+
+        }
+
+        return true;
+
+    }
+    
+}
