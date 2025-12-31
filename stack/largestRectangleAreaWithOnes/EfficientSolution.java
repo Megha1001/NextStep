c165e1201b432b53b1c0a10ff0f2f2987fc41855
Megha Verma <meghaverma1001@gmail.com>
2025-12-31 14:00:35 +0530

    Update EfficientSolution.java

--- stack/largestRectangleAreaWithOnes/EfficientSolution.java
+++ stack/largestRectangleAreaWithOnes/EfficientSolution.java
@@ -97,3 +97,77 @@ public class EfficientSolution {
     }
     
 }
+
+/*
+ * LEETCODE
+ * class Solution {
+    public int maximalRectangle(char[][] matrix) {
+
+        if(matrix.length == 0){
+            return 0;
+        }
+
+        int res = 0;
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        int hist[] = new int[c];
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c ; j++){
+                if(matrix[i][j] == '1'){ //character comparision
+                    hist[j] += 1;
+                }else{
+                    hist[j] = 0;
+                }
+            }
+            res = Math.max(res, largestHistogramArea(hist, c));
+        }
+
+        return res;
+    }
+
+    public static int largestHistogramArea(int arr[], int n){
+        int res = 0;
+        ArrayDeque<Integer> s = new ArrayDeque<>();
+        int r[] = new int[n];
+        int l[] = new int[n];
+
+        //for right smaller
+        for(int i=n-1; i>=0; i--){
+            while(s.size()> 0 && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+
+            r[i] = s.isEmpty() ? n : s.peek();
+            s.push(i);
+        }
+
+        while(!s.isEmpty()){
+            s.pop();
+        }
+
+        //for left smaller
+        for(int i=0; i<n; i++){
+            while(s.size() > 0 && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+
+            l[i] = s.isEmpty() ? -1 : s.peek();
+            s.push(i);
+        }
+
+
+        for(int i=0; i<n; i++){
+            int width = arr[i];
+            int length = r[i] - l[i] - 1;
+            int area = length*width;
+            res = Math.max(res, area);
+        }
+        
+        return res;
+
+    }
+}
+ */
