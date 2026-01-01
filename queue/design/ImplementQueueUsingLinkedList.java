96e4844f6b39b8bbacae2231b41e098c154f1f7c
Megha Verma <meghaverma1001@gmail.com>
2026-01-01 13:07:47 +0530

    Create ImplementQueueUsingLinkedList.java

---
+++ queue/design/ImplementQueueUsingLinkedList.java
@@ -0,0 +1,95 @@
+package queue.design;
+
+public class ImplementQueueUsingLinkedList {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    static class Queue{
+        Node front;
+        Node rear;
+        int size;
+
+        Queue(){
+            front = rear = null;
+            size = 0;
+        }
+
+        boolean isEmpty(){
+            if(size==0){
+                return true;
+            }
+
+            return false;
+        }
+
+
+        void enqueue(int x){
+            Node newNode = new Node(x);
+            if(isEmpty()){
+                ++size;
+                front = rear = newNode;
+                return;
+            }
+
+            ++size;
+            rear.next = newNode;
+            rear = newNode;
+        }
+
+        int dequeue(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            --size;
+            int val = front.data;
+            front = front.next;
+
+            return val;
+        }
+
+        int getFront(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return front.data;
+        }
+
+        int getRear(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return rear.data;
+        }
+
+    }
+
+    public static void main(String args[]){
+        Queue queue = new Queue(); 
+
+		queue.enqueue(10); 
+		queue.enqueue(20); 
+		queue.enqueue(30); 
+		queue.enqueue(40); 
+
+		System.out.println(queue.dequeue() 
+						+ " dequeued from queue\n"); 
+
+		System.out.println("Front item is "
+						+ queue.getFront()); 
+
+		System.out.println("Rear item is "
+						+queue.getRear());
+
+    }
+    
+}
