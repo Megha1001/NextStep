acd2b5bd3943bd2a471d0f2902716f48d9883f9f
Megha Verma <meghaverma1001@gmail.com>
2026-01-01 17:28:01 +0530

    Update NaiveSolution.java

--- queue/GenerateNumbersWithGivenDigits/NaiveSolution.java
+++ queue/GenerateNumbersWithGivenDigits/NaiveSolution.java
@@ -4,6 +4,9 @@ package queue.GenerateNumbersWithGivenDigits;
  * Question : Given a number n, print first n numbers(in increasing order) such that all the numbers have digits in set {5,6}
  * For example : if n=10;
  *  5,6,55,56,65,66,555,556,565,566 etc
+ * 
+ * 
+ * LIMITATION : NOT SUITABLE with big numbers
  */
 
 public class NaiveSolution {
@@ -24,7 +27,6 @@ public class NaiveSolution {
         while(count!=n){
             ++temp;
             int temp2 = temp;
-
             while(temp2!=0){
                 if(temp2%10 !=5 && temp2%10!=6){
                     break;
@@ -38,7 +40,6 @@ public class NaiveSolution {
             }
 
         }
-
 
     }
     
