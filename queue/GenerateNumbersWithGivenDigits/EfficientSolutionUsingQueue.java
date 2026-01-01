bdef7c04921fb9ac044af4d62ccc9742ff76c6a8
Megha Verma <meghaverma1001@gmail.com>
2026-01-01 17:26:49 +0530

    Create EfficientSolutionUsingQueue.java

---
+++ queue/GenerateNumbersWithGivenDigits/EfficientSolutionUsingQueue.java
@@ -0,0 +1,34 @@
+package queue.GenerateNumbersWithGivenDigits;
+
+/*
+ * Question : Given a number n, print first n numbers(in increasing order) such that all the numbers have digits in set {5,6}
+ * For example : if n=10;
+ *  5,6,55,56,65,66,555,556,565,566 etc
+ * 
+ * IDEA : Use Queue and store numbers in form of string
+ *  1. store 5, 6 
+ *  2. then poll 5 and print and appent 5,6 to it then again push to queue
+ *
+ */
+
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+public class EfficientSolutionUsingQueue {
+
+    public static void main(String args[]){
+        // int n = 10;
+        int n = 4;
+        System.out.println("The numbers are  ");
+        printNumbers(n);
+    }
+
+    public static void printNumbers(int n){
+        Queue<String> q = new ArrayDeque<>(); //string is intentional
+        for(int i=0; i<n; i++){
+
+        }
+
+    }
+    
+}
