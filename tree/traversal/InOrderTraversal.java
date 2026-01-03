ef7ad910ebad68fb1d11f4910e72e3e885232ffb
Megha Verma <meghaverma1001@gmail.com>
2026-01-03 18:34:46 +0530

    Create InOrderTraversal.java

---
+++ tree/traversal/InOrderTraversal.java
@@ -0,0 +1,38 @@
+package tree.traversal;
+
+public class InOrderTraversal {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(4);
+        root.right.right = new Node(5);
+
+        System.out.print("The inOrder traversal of given tree is : ");
+        inOrderTraversal(root);
+    }
+
+    public static void inOrderTraversal(Node root){ //LNR
+    
+        if(root != null){
+            inOrderTraversal(root.left);
+            System.out.print(root.data + " "); 
+            inOrderTraversal(root.right);
+        }
+
+        
+
+    }
+    
+}
