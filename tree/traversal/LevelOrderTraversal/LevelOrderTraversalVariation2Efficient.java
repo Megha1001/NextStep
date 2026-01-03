4ed2ba09960fc9b4260e8bd757b38cb71509152f
Megha Verma <meghaverma1001@gmail.com>
2026-01-03 21:57:32 +0530

    Create LevelOrderTraversalVariation2Efficient.java

---
+++ tree/traversal/LevelOrderTraversal/LevelOrderTraversalVariation2Efficient.java
@@ -0,0 +1,70 @@
+package tree.traversal.LevelOrderTraversal;
+
+
+import java.util.ArrayDeque;
+
+public class LevelOrderTraversalVariation2Efficient {
+    
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        // Node root = new Node(10);
+        // root.left = new Node(20);
+        // root.right = new Node(30);
+        // root.left.left = new Node(8);
+        // root.left.right = new Node(7);
+        // root.left.right.left = new Node(9);
+        // root.left.right.right = new Node(15);
+        // root.right.right = new Node(6);
+
+        // Node root = new Node(3);
+        // root.left = new Node(4);
+        // root.left.right = new Node(5);
+        
+        Node root = new Node(8);
+        root.right = new Node(6);
+        root.right.left = new Node(2);
+        root.right.right = new Node(4);
+        root.right.right.left = new Node(3);
+
+
+        System.out.println("Level order traversal of given tree is : ");
+        printLevelOrderTraversal(root);
+    }
+
+    public static void printLevelOrderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+
+        ArrayDeque<Node> q = new ArrayDeque<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int levelSize = q.size();
+
+            for(int i=0; i<levelSize; i++){
+                Node temp = q.poll();
+                System.out.print(temp.data + " ");
+
+                if(temp.left != null){
+                    q.offer(temp.left);
+                }
+
+                if(temp.right != null){
+                    q.offer(temp.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
