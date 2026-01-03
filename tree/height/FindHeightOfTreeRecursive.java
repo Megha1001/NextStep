f4613a4a310ef07857369f275f01a59714767616
Megha Verma <meghaverma1001@gmail.com>
2026-01-03 19:13:48 +0530

    Create FindHeightOfTreeRecursive.java

---
+++ tree/height/FindHeightOfTreeRecursive.java
@@ -0,0 +1,38 @@
+package tree.height;
+
+/*
+ * Height : Longest path from root to leaf
+ */
+
+public class FindHeightOfTreeRecursive {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(10);
+        root.left = new Node(8);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        System.out.println("Height of the given binary tree is : "+findHeight(root));
+    }
+
+    public static int findHeight(Node root){
+
+        if(root == null){
+            return 0;
+        }
+
+        return Math.max(findHeight(root.left), findHeight(root.right)) + 1;
+    }
+    
+}
