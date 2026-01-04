5cd6b6a7408380d05ce16cd5db8d15a012392626
Megha Verma <meghaverma1001@gmail.com>
2026-01-04 14:22:36 +0530

    Update CheckBalancedTreeUsingLevelOrderTraversalAndHeight.java

--- tree/isBalancedTree/CheckBalancedTreeUsingLevelOrderTraversalAndHeight.java
+++ tree/isBalancedTree/CheckBalancedTreeUsingLevelOrderTraversalAndHeight.java
@@ -3,6 +3,9 @@ package tree.isBalancedTree;
 /*
  * IDEA : For each node find height of level subtree and right subtree. If Math.abs(heigh of LST - height of RST) >0 return false;
  * For tree traversal use Level order traversal
+ * 
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : O(W) where w is width of tree
  */
 
 import java.util.ArrayDeque;
