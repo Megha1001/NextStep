a0233011cd23ef0ba9c531be5843d4e8f2d1c6e7
Megha Verma <meghaverma1001@gmail.com>
2026-01-06 20:51:47 +0530

    Create SearchInBSTIterative.java

---
+++ BST/search/SearchInBSTIterative.java
@@ -0,0 +1,50 @@
+package BST.search;
+
+public class SearchInBSTIterative {
+
+    static class TreeNode{
+        int key;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int key){
+            this.key = key;
+        }
+    }
+
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(15);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(18);
+        root.right.right = new TreeNode(80);
+        root.right.left.left = new TreeNode(16);
+
+        int k = 16;
+        System.out.println("is the given element present in BST ? "+isGivenNodePresent(root, k));
+    }
+
+    public static boolean isGivenNodePresent(TreeNode root, int k){
+        if(root==null){
+            return false;
+        }
+
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.key == k){
+                return true;
+            }
+            else if (curr.key > k){
+                //left
+                curr = curr.left;
+            }else{
+                //right
+                curr = curr.right;
+            }
+        }
+
+        return false;
+    }
+    
+}
