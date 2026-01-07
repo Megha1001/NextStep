ae6f78e9d6a0e892769ec416aef53cc23b17631b
Megha Verma <meghaverma1001@gmail.com>
2026-01-07 21:45:12 +0530

    Create CelingOnLeftUsingTreeSet.java

---
+++ BST/ceilingOnLeft/CelingOnLeftUsingTreeSet.java
@@ -0,0 +1,28 @@
+package BST.ceilingOnLeft;
+
+import java.util.TreeSet;
+
+public class CelingOnLeftUsingTreeSet {
+
+    public static void main(String args[]){
+        int arr[] = {2,8,30,15,25,12};
+        System.out.println("Ceiling on left side for every element is : ");
+        printCeilOnLeft(arr, arr.length);
+    }
+
+    public static void printCeilOnLeft(int arr[], int n){
+        System.out.print(-1+" ");
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(arr[0]);
+        
+        for(int i=1; i<n; i++){
+            if(treeSet.ceiling(arr[i])!=null){
+                System.out.print(treeSet.ceiling(arr[i])+" ");
+            }else{
+                System.out.print(-1+" ");
+            }
+            treeSet.add(arr[i]);
+        }
+    }
+
+}
