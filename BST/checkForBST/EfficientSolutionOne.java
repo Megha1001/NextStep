f8ffee219cfe39b3e5c1047f57fb764000e6beaf
Megha Verma <meghaverma1001@gmail.com>
2026-01-08 20:38:12 +0530

    Update EfficientSolutionOne.java

--- BST/checkForBST/EfficientSolutionOne.java
+++ BST/checkForBST/EfficientSolutionOne.java
@@ -23,13 +23,19 @@ public class EfficientSolutionOne {
     }
 
     public static void main(String args[]){
-        Node root = new Node(80);
-        root.left = new Node(70);
-        root.right = new Node(90);
-        root.left.left = new Node(60);
-        root.left.right = new Node(75);
-        root.right.left = new Node(85);
-        root.right.right = new Node(100);
+        // Node root = new Node(80);
+        // root.left = new Node(70);
+        // root.right = new Node(90);
+        // root.left.left = new Node(60);
+        // root.left.right = new Node(75);
+        // root.right.left = new Node(85);
+        // root.right.right = new Node(100);
+
+        Node root = new Node(20);
+        root.left = new Node(8);
+        root.right = new Node(30);
+        root.right.left = new Node(18);
+        root.right.right = new Node(35);
 
 
         //If your node values are int your BST should bound to long
