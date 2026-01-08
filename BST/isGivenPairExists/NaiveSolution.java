8350ce4ef116443b05ad341d40e47f1b8e030f47
Megha Verma <meghaverma1001@gmail.com>
2026-01-08 22:57:14 +0530

    Update NaiveSolution.java

--- BST/isGivenPairExists/NaiveSolution.java
+++ BST/isGivenPairExists/NaiveSolution.java
@@ -4,6 +4,8 @@ package BST.isGivenPairExists;
  * DO inorder traversal and use two pointer approach to find sum
  */
 
+import java.util.ArrayList;
+
 public class NaiveSolution {
 
     static class Node{
@@ -19,13 +21,53 @@ public class NaiveSolution {
     public static void main(String args[]){
         Node root = new Node(10);
         root.left = new Node(5);
-        root.right = new Node (16);
+        root.right = new Node (20);
         root.right.left = new Node(16);
         root.right.right = new Node(40);
 
         int sum  = 21;
 
         System.out.println("is the given sum exists in tree ? "+isPairSum(root, sum));
+    }
+
+    public static boolean isPairSum(Node root, int sum){
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrderTraversal(root, list);
+
+        for(int i :list){
+            System.out.print(i + " ");
+        }
+        return isSumExists(list, sum);
+    }
+
+    public static void inOrderTraversal(Node root, ArrayList<Integer> list){
+        if(root != null){
+            //LNR
+            inOrderTraversal(root.left, list);
+            list.add(root.data);
+            inOrderTraversal(root.right, list);
+        }
+    }
+
+    public static boolean isSumExists(ArrayList<Integer> list, int sum){
+        int i=0;
+        int j=list.size()-1;
+
+        while(i < j){
+            int x = list.get(i) + list.get(j);
+            if( x== sum){
+                return true;
+            }
+            else if(x > sum){
+                --j;
+            }
+            else{
+                ++i;
+            }
+        }
+
+        return false;
     }
     
 }
