d92c14299d9b9bb3d307070fe010b14d219bb044
Megha Verma <meghaverma1001@gmail.com>
2026-01-12 21:31:28 +0530

    Update AdjacencyList.java

--- graphs/representations/AdjacencyList.java
+++ graphs/representations/AdjacencyList.java
@@ -7,7 +7,7 @@ public class AdjacencyList {
     public static void main(String args[]){
         int V = 4;
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
 
         for(int i=0; i<V; i++){
             adj.add(i, new ArrayList<Integer>());
