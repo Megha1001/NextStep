86930d6e52ce6a33f3ad8437058ccac0428126c1
Megha Verma <meghaverma1001@gmail.com>
2026-01-14 00:25:02 +0530

    Create DetectCycleUsingDFS.java

---
+++ graph/detectCycle/DetectCycleUsingDFS.java
@@ -0,0 +1,9 @@
+package graph.detectCycle;
+
+/*
+ * IDEA : DO DFS for every node as source, and if we found the source again while traversing will say it has cycle
+ */
+
+public class DetectCycleUsingDFS {
+    
+}
