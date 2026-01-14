9d3cc91332043c71f328ce613e25ae2ea47c1652
Megha Verma <meghaverma1001@gmail.com>
2026-01-14 19:39:50 +0530

    Create TopologicalSortingUsingDFS.java

---
+++ graph/topologicalSorting/TopologicalSortingUsingDFS.java
@@ -0,0 +1,23 @@
+package graph.topologicalSorting;
+
+
+/*
+ * IDEA : Do DFS Traversal and after every vertex get visility push that in stack
+ * if u-> v then u should come before v
+ */
+
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+
+public class TopologicalSortingUsingDFS {
+
+    public static void main(String args[]){
+
+        int V = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for()
+
+    }
+    
+}
