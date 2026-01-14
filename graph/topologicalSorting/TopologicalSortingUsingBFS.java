ae6bbd0d1742d3ad5d35ac9f9389a7ece7dd0136
Megha Verma <meghaverma1001@gmail.com>
2026-01-14 15:18:21 +0530

    Update TopologicalSortingUsingBFS.java

--- graph/topologicalSorting/TopologicalSortingUsingBFS.java
+++ graph/topologicalSorting/TopologicalSortingUsingBFS.java
@@ -14,6 +14,7 @@ package graph.topologicalSorting;
  */
 
 import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class TopologicalSortingUsingBFS {
 
@@ -24,6 +25,58 @@ public class TopologicalSortingUsingBFS {
         for(int i=0; i<V; i++){
             adj.add(new ArrayList<>());
         }
+
+        addAnEdge(adj,0,2);
+        addAnEdge(adj,0,3);
+        addAnEdge(adj,2,3);
+        addAnEdge(adj,1,3);
+        addAnEdge(adj,1,4);
+
+        System.out.println("Topological sorting : ");
+        topologicalSorting(adj, V);
+    }
+
+    public static void topologicalSorting(ArrayList<ArrayList<Integer>>adj, int v){
+        int [] indegree = new int[v];
+        calculateIndgree(adj, indegree);
+        
+        //put all vertex with indegree 0 in queue
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        for(int i=0; i<v; i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int p = q.poll();
+            System.out.print(p + " ");
+
+            for(int u : adj.get(p)){
+                --indegree[u];
+                if(indegree[u] == 0){
+                    q.offer(u);
+                }
+            }
+        }
+    }
+
+    public static void calculateIndgree(ArrayList<ArrayList<Integer>>adj, int [] indegree){
+        //calculate indegree
+        for(int i=0; i<adj.size(); i++){
+            for(int j=0; j<adj.get(i).size(); j++){
+                ++indegree[adj.get(i).get(j)];
+            }
+        }
+
+        // for(int i: indegree){
+        //     System.out.print(i + " ");
+        // }
+    }
+
+    public static void addAnEdge(ArrayList<ArrayList<Integer>>adj, int u, int v){
+        adj.get(u).add(v);
     }
 
 
