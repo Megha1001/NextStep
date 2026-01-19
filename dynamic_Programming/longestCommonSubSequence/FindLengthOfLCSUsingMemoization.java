46fe1553d1e5a424260ac9a04e3b5a60bcdf85af
Megha Verma <meghaverma1001@gmail.com>
2026-01-19 21:24:32 +0530

    Update FindLengthOfLCSUsingMemoization.java

--- dynamic_Programming/longestCommonSubSequence/FindLengthOfLCSUsingMemoization.java
+++ dynamic_Programming/longestCommonSubSequence/FindLengthOfLCSUsingMemoization.java
@@ -1,12 +1,41 @@
 package dynamic_Programming.longestCommonSubSequence;
 
+import java.util.Arrays;
+
 public class FindLengthOfLCSUsingMemoization {
+
+    public static int memo[][];
 
     public static void main(String args[]){
         String s1 = "ABX";
         String s2 = "ABC";
 
+        memo = new int[s1.length()][s1.length()];
+
+        Arrays.fill(memo, -1);
+
         System.out.println("The length of LCS is : "+findLengthOfLCS(s1, s2, s1.length(), s2.length()));
+    }
+
+    public static int findLengthOfLCS(String s1, String s2, int m, int n){
+
+        if(memo[m][n] != -1){
+            return memo[m][n];
+        }
+
+        if(m==0 || n==0){
+            memo[m][n] = 0;
+        }
+        else {
+            if(s1.charAt(m-1) == s2.charAt(n-1)){
+                memo[m][n] = 1 + findLengthOfLCS(s1, s2, m-1, n-1);
+            }
+            else {
+                memo[m][n] = Math.max()
+            }
+        }
+
+
     }
     
 }
