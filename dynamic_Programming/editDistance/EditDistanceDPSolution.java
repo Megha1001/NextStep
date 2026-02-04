d6c5dc2051929f4835ed6f39f73cdf436d875687
Megha Verma <meghaverma1001@gmail.com>
2026-02-04 19:58:39 +0530

    Create EditDistanceDPSolution.java

---
+++ dynamic_Programming/editDistance/EditDistanceDPSolution.java
@@ -0,0 +1,41 @@
+package dynamic_Programming.editDistance;
+
+public class EditDistanceDPSolution {
+
+    public static void main(String args[]){
+        String s1 = "cut";
+        String s2 = "cat";
+        System.out.println("Edit distance for the two given strings is : "+findEditDistance(s1, s2, s1.length(), s2.length()));
+    }
+
+    public static int findEditDistance(String s1, String s2, int m, int n){
+        int dp[][] = new int[m+1][n+1];
+
+        //for first row
+        for(int i = 0; i <= m; i++){
+            dp[0][i] = i+1;
+        }
+
+        //for first column
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = i+1;
+        }
+
+
+        //for remaining position
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+                }
+            }
+        }
+
+
+        return dp[m][n];
+
+    }
+    
+}
